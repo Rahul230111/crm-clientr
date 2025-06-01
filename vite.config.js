41ea249a5a6501c16f18d69b3d924a27c98da9d3
Arul0107 <arulprakshv0107@gmail.com>
2025-06-01 11:39:46 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'https://crmbackend-w0lw.onrender.com',
         changeOrigin: true,
         secure: false
       }
