4418d3867d5c8fffb303d1f308459308becd12f2
Acculer2025 <acculermediatech@gmail.com>
2025-07-14 13:11:05 +0530

    use render as backend url

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 
     proxy: {
       '/api': {
-        target: 'https://crmbackend-production-911d.up.railway.app',
+        target: 'https://megacrane-crm.onrender.com',
         changeOrigin: true,
         secure: false
       }
